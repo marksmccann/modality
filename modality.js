311135eadbe21368fb213846d2db5f0ec23fe849
Mark McCann <mail@markmccann.me>
2014-12-23 13:28:25 -0600

    Update
    1. Dropped IE support, now IE10+ compatible (jQuery version will be IE friendly)
    2. Cleaned up and organized code.
    3. Removed triggers array from default options, added 'data-modality' attribute
    4. Added setTrigger method to bind a trigger on demand

--- modality.js
+++ modality.js
@@ -11,259 +11,82 @@ var Modality = (function () {
      */
     var extend = function () {
         var a = arguments;
-        for(var i in a) {
-            for(var key in a[i]) {
-                if(a[i].hasOwnProperty(key)) {
+        for( var i in a ) {
+            for( var key in a[i] ) {
+                if( a[i].hasOwnProperty(key) ) {
                     a[0][key] = a[i][key];
                 }
             }
         }
         return a[0];
-    },
-
-    // -----------------------------------------------------------------
-
-    /**
-     * returns the value of a given attribute of a given dom object
-     * @param  {dom object} node
-     * @param  {string}     attr
-     * @return {string} 
-     */
-    attr = function (node, attr) {
-        if (node.hasAttribute(attr)) return node.getAttribute(attr);
-    },
-
-    // -----------------------------------------------------------------
-
-    /**
-     * removes duplicates from an array
-     * @param  {array} array 
-     * @return {array} 
-     */
-    uniqueArray = function (array) {
-        var a = array.concat();
-        for(var i=0; i < a.length; ++i) {
-            for(var j=i+1; j<a.length; ++j) {
-                if(a[i] === a[j]) {
-                    a.splice(j--, 1);
-                }
-            }
-        }
-        return a;
-    },
-
-    // -----------------------------------------------------------------
-
-    /**
-     * binds an event listener to a given dom object
-     * @param  {dom object} node 
-     * @param  {string}     event
-     * @param  {function}   callback
-     */
-    bind = function( target, event, fn ) {
-        if ( target.attachEvent ) {
-            target['e'+event+fn] = fn;
-            target[event+fn] = function(){ target['e'+event+fn]( window.event ); }
-            target.attachEvent( 'on'+event, target[event+fn] );
-        } else {
-           target.addEventListener( event, fn, false );
-        }
-    },
-
-    // -----------------------------------------------------------------
-
-    /**
-     * inserts a new dom element after a given target element
-     * @param  {dom object} newElement - the object you want to add
-     * @param  {dom object} target - the object you want it added after
-     */
-    insertAfter = function (newElement,target) {
-        var parent = target.parentNode;
-        if(parent.lastchild == target) {
-            parent.appendChild(newElement);
-        } else {
-            parent.insertBefore(newElement, target.nextSibling);
-        }
-    },
-
-    // -----------------------------------------------------------------
-
-    /**
-     * determine if a given node has a particular class
-     * @param {object} target - the node you are adding a class to
-     * @param {string} className - the class you are checking for
-     * @return {boolean} 
-     */
-    hasClass = function( target, className ) {
-        return target.className.match(new RegExp('(\\s|^)'+className+'(\\s|$)'));
-    },
-
-    /**
-     * add class to a given node
-     * @param {object} target - the node you are adding a class to
-     * @param {string} className - the class you are adding
-     */
-    addClass = function( target, className ) {
-        if( ! hasClass(target, className) ) {
-            target.className += " " + className;
-        }
-    },
-
-    /**
-     * remove class from a given node
-     * @param {object} target - the node you are removing a class from
-     * @param {string} className - the class you are removing
-     */
-    removeClass = function( target, className ) {
-        if( hasClass(target, className) ) {
-            var re = new RegExp("(\\s|^)" + className + "(\\s|$)", "g");
-            target.className = target.className.replace(re , '');
-        }
-    };
-
-    // -----------------------------------------------------------------
-
-    /**
-     * sets up the modal by wrapping markup in appropriate classes
-     * @param  {object} modal 
-     * @return {object} 
-     */
-    var wrap = function ( modal ) {
-        // create the new container
-        var wrapper = document.createElement('div');
-        wrapper.setAttribute('class', modal.options.modalClass);
-
-        // if ie, do a couple things
-        var ie = '';
-        if (/MSIE (\d+\.\d+);/.test(navigator.userAgent)) {
-            var ieVersion = new Number(RegExp.$1);
-            if(ieVersion <= 8) ie = '<div class="'+modal.options.ieClass+'"></div>';
-            if(ieVersion == 9) wrapper.setAttribute('style', "filter:none;");
-        }
-
-        // insert modal content into the new container
-        wrapper.innerHTML = ie + '<div class="'+modal.options.innerClass+'">' + modal.modal.outerHTML + '</div>';
-        insertAfter(wrapper, modal.modal);
-
-        // remove the old modal from the dom
-        modal.modal.parentNode.removeChild( modal.modal );
-
-        // add css3 effect class
-        addClass( wrapper, modal.options.effect );
-
-        // bind click event to close the modal on wrapper
-        if( modal.options.clickOffClose ) {
-            bind( wrapper, 'click', function(e) {
-                e.preventDefault();
-                if( e.target == wrapper ) modal.close();
-            });
-        }
-
-        return wrapper;
-    }
-
-    // -----------------------------------------------------------------
-
-    /**
-     * collects array of anchors whose 'href' mathces the 'id' of the target
-     * @param  {string} targetID 
-     * @return {array}          
-     */
-    anchors = function (targetID) {
-
-        // get all the anchor tags in document
-        var anchors = document.getElementsByTagName('a');
-
-        // loop through anchors and collect any that match criteria
-        var triggers = [];
-        for (var i = 0; i < anchors.length; i++) {
-            if (anchors[i].getAttribute("href") == '#'+targetID) {
-                triggers.push(anchors[i]);
-            }
-        }
-
-        return triggers;
-    },
-
-    // -----------------------------------------------------------------
-
-    /**
-     * collects all triggers and adds click events to each of them
-     * @param  {object} modal
-     * @return {array} 
-     */
-    triggers = function (modal) {
-
-        // create new array
-        var triggers = [];
-
-        // if auto bind set true, include in trigger array
-        if(modal.options.autoBind) {
-            triggers = modal.anchors;
-        }
-
-        // loop through passed-in triggers and add to array
-        for (var i in modal.options.triggers) {
-            triggers.push(modal.options.triggers[i]);
-        }
-
-        // remove any duplicate triggers
-        triggers = uniqueArray(triggers);
-
-        // bind click action for all triggers
-        for(var i in triggers) {
-            bind(triggers[i],"click",function (e) {
-                e.preventDefault();
-                modal.toggle();
-            });
-        }
-
-        return triggers;
-
     };
 
     // -----------------------------------------------------------------
 
     /**
      * the modality object
-     * @param {dom object} modal
-     * @param {object}     options
-     * @param {function}   callback
+     * @param {object} modal
+     * @param {object} options
+     * @param {function} callback
      */
-    var Modality = function (modal, options, callback) {
+    var Modality = function ( modal, options, callback ) {
 
-        // create local var to avoid scopr issues
+        // create local var to avoid scope issues
         var base = this;
 
         // set initial attributes
-        base.modal = modal;
-        base.id = attr(base.modal, 'id');
-        base.options = extend( {}, Modality.defaults, options);
-        base.body = document.getElementsByTagName( 'body' )[0];
+        base.id       = modal.getAttribute( 'id' );
+        base.options  = extend( {}, Modality.defaults, options );
+        base.body     = document.getElementsByTagName( 'body' )[0];
+        base.triggers = document.querySelectorAll( 'a[href="#'+base.id+'"], [data-modality="#'+base.id+'"]' );
 
-        // set up the modal in the dom
-        base.wrapper = wrap(base);
+        // build and set modal wrapper 
+        base.wrapper = document.createElement('div');
+        base.wrapper.setAttribute( 'class', base.options.modalClass + ' ' + base.options.effect );
+        base.wrapper.innerHTML = '<div class="'+base.options.innerClass+'">' + modal.outerHTML + '</div>';
+        modal.parentNode.replaceChild( base.wrapper, modal );
 
-        // set remaining attributes
-        base.modal = document.getElementById(base.id);
-        base.anchors = anchors(base.id);
-        base.triggers = triggers(base);
+        // set the original modal
+        base.modal = document.getElementById( base.id );
 
-        // open modal if auto open is set
-        if(base.options.autoOpen) base.open();
+        // --------------------------------------------
 
-        // run the callback function
-        if (typeof callback == 'function') { callback(); }
+        // toggle modal on all triggers
+        if( base.options.autoBind ) {
+            for( var i = 0; i < base.triggers.length; i++ ) {
+                base.setTrigger(base.triggers[i]);
+            }
+        }
 
-        // add this modal to list of modals on page
-        Modality.modals[base.id] = base;
+        // close modal if users clicks anywhere off of it
+        if( base.options.clickOffClose ) {
+            base.wrapper.addEventListener( "click", function(e) {
+                e.preventDefault(); if(e.target == base.wrapper) base.close();
+            }, false );
+        }
 
-        return base;
+        // close modal with 'esc' key
+        if( this.options.closeOnEscape ) {
+            base.body.addEventListener( "keyup", function (e) {
+                if(e.keyCode == 27){ base.close(); }
+            }, false);
+        }
+
+        if( base.options.autoOpen ) base.open(); 
+
+        // --------------------------------------------
+
+        if( typeof callback == 'function' ) callback();
+
+        return Modality.modals[base.id] = base;
 
     };
 
     // -----------------------------------------------------------------
 
+    /**
+     * class methods for the modality object
+     */
     Modality.prototype = {
 
         /**
@@ -271,15 +94,15 @@ var Modality = (function () {
          * @param  {function} callback
          * @return {instance} 
          */
-        close: function (callback) {
+        close: function ( callback ) {
 
-            // remove classes to open the modal 
-            removeClass(this.wrapper,this.options.openClass);
-            removeClass(this.body,this.options.openClass);
+            // remove classes to close the modal
+            this.wrapper.classList.remove( this.options.openClass );
+            this.body.classList.remove( this.options.openClass );
 
-            // run the callbacks
-            if ( typeof this.options.onClose == 'function' ) { this.options.onClose(); }
-            if ( typeof callback == 'function' ) { callback(); }
+            // run the callback(s)
+            if ( typeof this.options.onClose == 'function' ) this.options.onClose();
+            if ( typeof callback == 'function' ) callback();
 
             return this;
 
@@ -290,36 +113,27 @@ var Modality = (function () {
          * @param  {function} callback
          * @return {instance}
          */
-        open: function (callback) {
+        open: function ( callback ) {
 
             // add classes to open the modal
-            addClass(this.wrapper,this.options.openClass);
-            addClass(this.body,this.options.openClass);
+            this.wrapper.classList.add( this.options.openClass );
+            this.body.classList.add( this.options.openClass );
 
-            // bind keyup event to close modal
-            if(this.options.closeOnEscape) {
-                var modal = this;
-                bind(document.body,"keyup",function (e) {
-                    if(e.keyCode == 27){ modal.close(); }
-                });
-            }
-
-            // run the callback
-            if ( typeof this.options.onOpen == 'function' ) { this.options.onOpen(); }
-            if ( typeof callback == 'function' ) { callback(); }
+            // run the callback(s)
+            if ( typeof this.options.onOpen == 'function' ) this.options.onOpen();
+            if ( typeof callback == 'function' ) callback();
 
             return this;
 
         },
 
         /**
-         * opens modal if closed and vice versa
+         * toggles the modal
          * @param  {function} callback
          * @return {instance}
          */
-        toggle: function (callback) {
-            (this.isOpen()) ? this.close(callback) : this.open(callback);
-            return this;
+        toggle: function ( callback ) {
+            return ( this.isOpen() ) ? this.close(callback) : this.open(callback);
         },
 
         /**
@@ -327,37 +141,78 @@ var Modality = (function () {
          * @return {Boolean}
          */
         isOpen: function () {
-            return (hasClass(this.wrapper, this.options.openClass)) ? true : false;
+            return ( this.wrapper.classList.contains(this.options.openClass) ) ? true : false;
+        },
+
+        /**
+         * manually set trigger for a modal
+         * @param  {object} - the element you want to be the trigger
+         * @return {instance}
+         */
+        setTrigger: function ( trigger ) {
+
+            // set local var for instance
+            var base = this;
+
+            // set click event for new trigger
+            trigger.addEventListener( "click", function (e) {
+                e.preventDefault(); base.toggle();
+            }, false );
+
+            return this;
         }
+
     };
 
     // -----------------------------------------------------------------
     
+    /**
+     * initalizes modal(s)
+     * @param  {string} query
+     * @param  {object} options
+     * @param  {function} fn
+     * @return {object,array}
+     */
     Modality.init = function ( query, options, fn ) {
+
+        // collect the modals from the DOM
         var modals = document.querySelectorAll(query);
+
+        // loop through the modals and initialize each one
         for( var i = 0; i < modals.length; i++ ) {
-            new Modality(modals[i], options, fn);
+
+            // Initialize the modal
+            var modal = new Modality(modals[i], options, fn);
+
+            // return only this modal if only one
+            if(modals.length == 1) { return modal; }
         }
+
+        // return array of modals
         return Modality.modals;
     }
 
     // -----------------------------------------------------------------
 
+    /**
+     * list of Modality defaults
+     */
     Modality.defaults = {
-        modalClass: "modality-modal",
-        innerClass: "mm-wrap",
-        openClass: "mm-show",
-        ieClass: "mm-ghost",
-        clickOffClose: true,
-        closeOnEscape: true,
-        autoOpen: false,
-        autoBind: true,
-        triggers: [],
-        effect: "effect-1",
-        onOpen: function(){},
-        onClose: function(){}
+        modalClass: "modality-modal", // outer-most container
+        innerClass: "mm-wrap", // inner wrapper
+        openClass: "mm-show", // when modal is visible
+        clickOffClose: true, // click anywhere off of modal to close it
+        closeOnEscape: true, // close modal with 'esc' key
+        autoOpen: false, // open on page load
+        autoBind: true, // automatically bind triggers to modal
+        effect: "effect-1", // animation style
+        onOpen: function(){}, // function to run when modal opens
+        onClose: function(){} // function to run when modal closes
     }
 
+    /**
+     * an empty object to collect all modals on page
+     */
     Modality.modals = {};
 
     // -----------------------------------------------------------------
